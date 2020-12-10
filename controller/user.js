15474c9ed828b10080ca2b3507d6b61e8666c82d
syy <1101976384@qq.com>
2020-12-10 20:36:24 +0800

    1

---
+++ controller/user.js
@@ -0,0 +1,28 @@
+const {
+    exec,
+    escape
+} = require('../db/mysql')
+const {
+    genPassword
+} = require('../utils/cryp')
+
+const login = async (username, password) => {
+    username = escape(username)
+
+    // // 生成加密密码
+    password = genPassword(password)
+    password = escape(password)
+    console.log('password----', password)
+    const sql = `
+        select username, realname from users where username=${username} and password=${password}
+    `
+    // console.log('sql is', sql)
+
+    const rows = await exec(sql)
+    console.log('rows----', rows)
+    return rows[0] || {}
+}
+
+module.exports = {
+    login
+}
